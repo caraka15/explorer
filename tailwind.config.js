0a723c1f05dd51d8d570ecc089d4c32a6d4b8b4f
carakawidi07 <carakawidi07@gmail.com>
2024-06-17 14:34:50 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,16 +19,35 @@ module.exports = {
     themes: [
       {
         light: {
-          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
-          primary: '#666cff',
+          "color-scheme": "light",
+          "primary": "#ff9800",         // Oranye sebagai warna utama
+          "primary-content": "#ffffff", // Konten utama kontras dengan oranye
+          "secondary": "#f000b8",
+          "secondary-content": "#FFD1F4",
+          "accent": "#1ECEBC",
+          "accent-content": "#07312D",
+          "neutral": "#2B3440",
+          "neutral-content": "#D7DDE4",
+          "base-100": "#ffffff",
+          "base-200": "#FFF3E0",        // Warna dasar 200 dengan sentuhan oranye
+          "base-300": "#FFE0B2",        // Warna dasar 300 dengan sentuhan oranye
+          "base-content": "#1f2937",
         },
-      },
-      {
         dark: {
-          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
-          primary: '#666cff',
-          'base-100': '#2a334c',
-          'base-200': '#252d37'
+          "color-scheme": "dark",
+          "primary": "#ff9800",         // Oranye sebagai warna utama
+          "primary-content": "#ffffff", // Konten utama kontras dengan oranye
+          "secondary": "#D926AA",
+          "secondary-content": "#ffffff",
+          "accent": "#1FB2A5",
+          "accent-content": "#ffffff",
+          "neutral": "#2a323c",
+          "neutral-focus": "#242b33",
+          "neutral-content": "#A6ADBB",
+          "base-100": "#1d232a",
+          "base-200": "#292D32",        // Warna dasar 200 dengan sentuhan gelap oranye
+          "base-300": "#3A3E44",        // Warna dasar 300 dengan sentuhan gelap oranye
+          "base-content": "#A6ADBB",
         },
       },
     ],
