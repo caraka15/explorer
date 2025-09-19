79148abf1b1dfa28f357d84e8ad6743a99f408df
caraka15 <carakawidi07@gmail.com>
2025-09-19 17:46:34 +0700

    add paxi chain

--- src/stores/useDashboard.ts
+++ src/stores/useDashboard.ts
@@ -58,7 +58,7 @@ export function convertFromLocal(lc: LocalChainConfig): ChainConfig {
     };
   }
   conf.features = lc.features;
-  conf.logo = lc.logo.startsWith('http') ? lc.logo : `https://crxanode.me${lc.logo}`;
+  conf.logo = lc.logo.startsWith('http') ? lc.logo : `https://explorer.crxanode.me${lc.logo}`;
   conf.keplrFeatures = lc.keplr_features;
   conf.keplrPriceStep = lc.keplr_price_step;
   conf.themeColor = lc.theme_color;
